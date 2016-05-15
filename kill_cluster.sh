2f55a6fbabe708e3afa07f7902a2b5f062d3ee17
Christian Mikkelsen <chmi@mvno.dk>
2016-05-15 23:32:08 +0200

    Rework

--- kill_cluster.sh
+++ kill_cluster.sh
@@ -1,18 +1,5 @@
-echo "Killing Kafka 1"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
-echo "Killing Kafka 2"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
-echo "Killing Kafka 3"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
-echo "Killing Zookeeper 1"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
-echo "Killing Zookeeper 2"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
-echo "Killing Zookeeper 3"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -9
-sleep 1
+echo "Killing all java processes..."
+ps ax | grep java | grep -v grep | awk '{print $1}'
+ps ax | grep java | grep -v grep | awk '{print $1}' | sudo xargs kill -9
+sleep 3
+ps ax | grep java | grep -v grep | awk '{print $1}' | wc -l
--- stop_cluster.sh
+++ stop_cluster.sh
@@ -1,18 +1,8 @@
-echo "Stopping Kafka 1"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
-echo "Stopping Kafka 2"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
-echo "Stopping Kafka 3"
-ps ax | grep -i 'kafkaServer-gc.log' | grep java | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
-echo "Stopping Zookeeper 1"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
-echo "Stopping Zookeeper 2"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
-echo "Stopping Zookeeper 3"
-ps ax | grep -i 'zookeeper-gc.log' | grep -v grep | awk '{print $1}' | head -1 | sudo xargs kill -SIGTERM
-sleep 1
+./shutdown_random_kafka.sh
+./shutdown_random_kafka.sh
+./shutdown_random_kafka.sh
+./shutdown_random_zookeeper.sh
+./shutdown_random_zookeeper.sh
+./shutdown_random_zookeeper.sh
+
+./kill_cluster.sh
