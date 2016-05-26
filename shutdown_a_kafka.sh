2612101a710dab8b5196a5a0ca941e31b00a9ef9
Christian Mikkelsen <chmi@mvno.dk>
2016-05-26 14:21:26 +0200

    added shutdown k + zk by number

---
+++ shutdown_a_kafka.sh
@@ -0,0 +1,22 @@
+pid=$(ps -o pid,cmd ax | grep -i ".*/etc/kafka$1" | grep -v grep | xargs | cut -d \  -f 1)
+echo "Stopping Kafka$1 ($pid)"
+echo $pid | sudo xargs kill -SIGTERM
+
+attempt=0
+until [ $attempt -ge 20 ]
+do
+	if [ ps -p $pid > /dev/null ] ; then
+		echo "  - $pid not stopped yet... $attempt"
+		sleep 1
+	else
+		break
+	fi
+
+	attempt=$(($attempt+1))
+done
+
+if [ ps -p $pid > /dev/null ] ; then
+	echo "  - Warning: $pid was not stopped, using kill 9..."
+	sudo kill -9 $pid
+	sleep 1
+fi
