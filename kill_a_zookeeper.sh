d87231bbe8a7a6ef81155f309457a6b600afe628
Christian Mikkelsen <chmi@mvno.dk>
2016-05-26 14:21:43 +0200

    Added kill by

---
+++ kill_a_zookeeper.sh
@@ -0,0 +1,22 @@
+pid=$(ps -o pid,cmd ax | grep -i ".*/etc/zookeeper$1" | grep -v grep | xargs | cut -d \  -f 1)
+echo "Killing Zookeeper$1 ($pid)"
+echo $pid | sudo xargs kill -9
+
+attempt=0
+until [ $attempt -ge 20 ]
+do
+	if [ ps -p $pid > /dev/null ] ; then
+		echo "  - $pid not stopped yet... $attempt"
+		sleep 1
+	else
+		break
+	fi
+
+	attempt=$(($attempt+1))
+done
+
+if [ ps -p $pid > /dev/null ] ; then
+	echo "  - Warning: $pid was not stopped, using kill 9..."
+	sudo kill -9 $pid
+	sleep 1
+fi
