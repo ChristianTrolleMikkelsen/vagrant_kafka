dd8d505ef590cf61a0e2c7fb856fed6e95ebbc3c
Christian Mikkelsen <chmi@mvno.dk>
2016-09-20 12:50:19 +0200

    else if

--- start_cluster.sh
+++ start_cluster.sh
@@ -12,47 +12,12 @@ if [ $numberOfProcesses -eq 6 ] ; then
 	echo "Kafka 2:		192.168.100.100:9092"
 	echo "Kafka 3:		192.168.100.100:9093"
 	break
-fi
+else
 
-attempt=0
-until [ $attempt -ge 3 ]
-do
-	echo "Cluster starting"
-	echo " - Launching Zookeeper 1"
-	sudo /etc/zookeeper1/bin/zookeeper-server-start.sh -daemon /etc/zookeeper1/config/zookeeper.properties
-	sleep 1
-	echo " - Launching Zookeeper 2"
-	sudo /etc/zookeeper2/bin/zookeeper-server-start.sh -daemon /etc/zookeeper2/config/zookeeper.properties
-	sleep 1
-	echo " - Launching Zookeeper 3"
-	sudo /etc/zookeeper3/bin/zookeeper-server-start.sh -daemon /etc/zookeeper3/config/zookeeper.properties
-	sleep 6
-	echo " - Launching Kafka 1"
-	sudo /etc/kafka1/bin/kafka-server-start.sh -daemon /etc/kafka1/config/server.properties
-	sleep 2
-	echo " - Launching Kafka 2"
-	sudo /etc/kafka2/bin/kafka-server-start.sh -daemon /etc/kafka2/config/server.properties
-	sleep 2
-	echo " - Launching Kafka 3"
-	sudo /etc/kafka3/bin/kafka-server-start.sh -daemon /etc/kafka3/config/server.properties
-	sleep 2
-
-	ps ax | grep java | grep -v grep | awk '{print $1}'
-	numberOfProcesses=$(ps ax | grep java | grep -v grep | awk '{print $1}' | wc -l)
-
-	if [ $numberOfProcesses -eq 6 ] ; then
-		echo "*************Cluster running***************"
-		echo "Ip: 192.168.100.100"
-		echo ""
-		echo "Zookeeper 1:	192.168.100.100:2181 (2888:3888)"
-		echo "Zookeeper 2:	192.168.100.100:2182 (2889:3889)"
-		echo "Zookeeper 3:	192.168.100.100:2183 (2890:3890)"
-		echo "Kafka 1:		192.168.100.100:9091"
-		echo "Kafka 2:		192.168.100.100:9092"
-		echo "Kafka 3:		192.168.100.100:9093"
-		break
-	else
-		echo "Cluster not started correctly, retrying $attempt"
+	attempt=0
+	until [ $attempt -ge 3 ]
+	do
+		echo "Cluster starting"
 		echo " - Launching Zookeeper 1"
 		sudo /etc/zookeeper1/bin/zookeeper-server-start.sh -daemon /etc/zookeeper1/config/zookeeper.properties
 		sleep 1
@@ -70,8 +35,44 @@ do
 		sleep 2
 		echo " - Launching Kafka 3"
 		sudo /etc/kafka3/bin/kafka-server-start.sh -daemon /etc/kafka3/config/server.properties
-		sleep 10
-	fi
+		sleep 2
 
-	attempt=$(($attempt+1))
-done
+		ps ax | grep java | grep -v grep | awk '{print $1}'
+		numberOfProcesses=$(ps ax | grep java | grep -v grep | awk '{print $1}' | wc -l)
+
+		if [ $numberOfProcesses -eq 6 ] ; then
+			echo "*************Cluster running***************"
+			echo "Ip: 192.168.100.100"
+			echo ""
+			echo "Zookeeper 1:	192.168.100.100:2181 (2888:3888)"
+			echo "Zookeeper 2:	192.168.100.100:2182 (2889:3889)"
+			echo "Zookeeper 3:	192.168.100.100:2183 (2890:3890)"
+			echo "Kafka 1:		192.168.100.100:9091"
+			echo "Kafka 2:		192.168.100.100:9092"
+			echo "Kafka 3:		192.168.100.100:9093"
+			break
+		else
+			echo "Cluster not started correctly, retrying $attempt"
+			echo " - Launching Zookeeper 1"
+			sudo /etc/zookeeper1/bin/zookeeper-server-start.sh -daemon /etc/zookeeper1/config/zookeeper.properties
+			sleep 1
+			echo " - Launching Zookeeper 2"
+			sudo /etc/zookeeper2/bin/zookeeper-server-start.sh -daemon /etc/zookeeper2/config/zookeeper.properties
+			sleep 1
+			echo " - Launching Zookeeper 3"
+			sudo /etc/zookeeper3/bin/zookeeper-server-start.sh -daemon /etc/zookeeper3/config/zookeeper.properties
+			sleep 6
+			echo " - Launching Kafka 1"
+			sudo /etc/kafka1/bin/kafka-server-start.sh -daemon /etc/kafka1/config/server.properties
+			sleep 2
+			echo " - Launching Kafka 2"
+			sudo /etc/kafka2/bin/kafka-server-start.sh -daemon /etc/kafka2/config/server.properties
+			sleep 2
+			echo " - Launching Kafka 3"
+			sudo /etc/kafka3/bin/kafka-server-start.sh -daemon /etc/kafka3/config/server.properties
+			sleep 10
+		fi
+
+		attempt=$(($attempt+1))
+	done
+fi
